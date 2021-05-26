2646d41a0fc5a96e07f4500242cca1de593b1b1d
unknown <quantran2381@gmail.com>
2021-05-26 15:15:03 +0700

    full stack react

--- EbookLapTrinhNodeJSThatDonGian/EbookLapTrinhNodeJSThatDonGian/clean-blog/index.js
+++ EbookLapTrinhNodeJSThatDonGian/EbookLapTrinhNodeJSThatDonGian/clean-blog/index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = new express();
 const path = require("path");
 
+const ejs = require("ejs");
+app.set("view engine", "ejs");
+
 //đăng ký thư mục public.
 app.use(express.static("public"));
 
@@ -11,5 +14,7 @@ app.listen(4000, () => {
 });
 
 app.get("/", (request, response) => {
-  response.sendFile(path.resolve(__dirname, "pages/index.html"));
+  // response.sendFile(path.resolve(__dirname, "pages/index.html"));
+  response.render("index");
 });
+
